5e43ffbaab4d51dbd47b79d844ba3b28d3b69ee3
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-02-02 12:40:18 +0100

    feat(home-screen): implemented launches list UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spacex_app/cubit/launch_cubit.dart';
 import 'package:spacex_app/routes.dart';
+import 'package:spacex_app/services/spacex_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BlocProvider(
+    create: (context) => LaunchCubit(SpaceXService()),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:spacex_app/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spacex_app/cubit/launch_cubit.dart';
+// import 'package:spacex_app/routes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    context.read<LaunchCubit>().fetchAllLaunches();
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Home Screen'),
-            SizedBox(
-              width: 10,
-            ),
-            ElevatedButton(
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(Routes.singleLaunchScreen),
-              child: Text(
-                'Next Screen',
-              ),
-            )
-          ],
-        ),
+      appBar: AppBar(title: const Text("All Launches")),
+      body: BlocBuilder<LaunchCubit, LaunchState>(
+        builder: (context, state) {
+          if (state is LaunchLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is LaunchLoaded) {
+            return ListView.builder(
+              itemCount: state.launches.length,
+              itemBuilder: (context, index) {
+                final launch = state.launches[index];
+                return ListTile(
+                  title: Text(launch.name),
+                  subtitle: Text(launch.date.toString()),
+                );
+              },
+            );
+          } else if (state is LaunchError) {
+            return Center(child: Text(state.message));
+          }
+          return Container();
+        },
       ),
     );
   }
