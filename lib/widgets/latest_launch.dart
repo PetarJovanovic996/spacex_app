06d2aef2a70c601babcc575228ed54d2fceaae1c
Petar Jovanovic <petarjovanovic@Mac.home>
2025-02-07 18:39:19 +0100

    Refactor to use  SingleLaunchWidget

--- lib/widgets/latest_launch.dart
+++ lib/widgets/latest_launch.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spacex_app/cubit/past_cubit.dart';
 import 'package:spacex_app/widgets/loading_widget.dart';
+import 'package:spacex_app/widgets/single_launch_widget.dart';
 
 class LatestLaunch extends StatelessWidget {
   const LatestLaunch({super.key});
@@ -11,44 +12,11 @@ class LatestLaunch extends StatelessWidget {
     return BlocBuilder<PastCubit, PastState>(
       builder: (context, state) {
         if (state is PastLaunchLoading) {
-          state is LoadingWidget;
+          return LoadingWidget();
         }
         if (state is PastAndLatestLoaded) {
           final launch = state.latestLaunch;
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(launch.name),
-                    SizedBox(
-                      height: 6,
-                    ),
-                    Text("ID: ${launch.id}"),
-                    Text("Date: ${launch.formattedDate}"),
-                    Text("Success: ${launch.success ? 'Yes' : 'No'}"),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "Details: ${launch.details}",
-                      style: TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      maxLines: 3,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+          return SingleLaunchWidget(launch: launch);
         } else if (state is PastLaunchError) {
           return Center(child: Text(state.message));
         }
--- lib/widgets/past_launches.dart
+++ lib/widgets/past_launches.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spacex_app/cubit/past_cubit.dart';
 import 'package:spacex_app/widgets/loading_widget.dart';
+import 'package:spacex_app/widgets/single_launch_widget.dart';
 
 class PastLaunches extends StatelessWidget {
   const PastLaunches({
@@ -13,7 +14,7 @@ class PastLaunches extends StatelessWidget {
     return BlocBuilder<PastCubit, PastState>(
       builder: (context, state) {
         if (state is PastLaunchLoading) {
-          state is LoadingWidget;
+          return LoadingWidget();
         }
         if (state is PastAndLatestLoaded) {
           return Expanded(
@@ -26,40 +27,7 @@ class PastLaunches extends StatelessWidget {
               itemCount: state.pastLaunches.length,
               itemBuilder: (context, index) {
                 final launch = state.pastLaunches[index];
-                return Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Card(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(launch.name),
-                          SizedBox(
-                            height: 6,
-                          ),
-                          Text("ID: ${launch.id}"),
-                          Text("Date: ${launch.formattedDate}"),
-                          Text("Success: ${launch.success ? 'Yes' : 'No'}"),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Details: ${launch.details}",
-                            style: TextStyle(
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            maxLines: 3,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
+                return SingleLaunchWidget(launch: launch);
               },
             ),
           );
